7f0c97c6eef8c59baeded15d40fd83c32610849b
Maverick Manasco <MaverickCER@gmail.com>
2022-11-24 17:11:33 -0500

    Update hydrogen.config.js

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,15 +6,15 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     showQueryTiming: true,
     // @ts-ignore kitunga-fashion.myshopify.com
-    storeDomain: process?.env?.PUBLIC_STORE_DOMAIN,
+    storeDomain: Oxygen?.env?.PUBLIC_STORE_DOMAIN,
     // @ts-ignore Storefront API access token
-    storefrontToken: process?.env?.PUBLIC_STOREFRONT_API_TOKEN,
+    storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
     // @ts-ignore Admin API access token
-    privateStorefrontToken: process?.env?.PRIVATE_STOREFRONT_API_TOKEN,
+    privateStorefrontToken: Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
     // @ts-ignore
     storefrontApiVersion: '2022-10',
     // @ts-ignore
-    storefrontId: process?.env?.PUBLIC_STOREFRONT_ID,
+    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
   },
   session: CookieSessionStorage('__session', {
     path: '/',
